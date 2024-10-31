918f7c20f493109fb4c61fcf3bb63e9731200c17
MM109 <daanishm8@gmail.com>
2024-10-31 12:42:58 +0530

    modified home and added about component

--- src/app/about/components/about/about.component.ts
+++ src/app/about/components/about/about.component.ts
@@ -1,21 +1,83 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [UpperCasePipe],
+  imports: [HeaderComponent],
   template: `
-  <div>
-    <h1>About us</h1>
-    <p>Pipes Demo {{randomTextsd}}</p>
-    <p>Pipe Usage | {{randomTextsd | uppercase }}</p>
+  <div class="bg-about px-3">
+  <div class="px-4 py-5 my-3 mt-0 text-center text-white ">
+    <h1 class="display-3 text-white pb-5 fw-light">
+      About
+    </h1>
+    <h3 class="display-5 text-white pb-5 fw-semibold">We are an online language learning community lorem ipsum dolor sit amet.</h3>
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+      class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+      <path
+        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+    </svg>
+    <i class="bi bi-arrow-down-circle-fill"></i>
   </div>
+</div>
+  <div class="container-fluid">
+  <!-- Main Content -->
+  <div id="content" class="site-content py-5">
+    <div class="container">
+      <main id="main" class="site-main p-4">
+        <article class="mb-5 pt-3">
+          
+          <div class="mb-5">
+            <h6 class="text-danger text-center fw-light pb-2">About us</h6>
+            <h2 class="fw-bold  text-center">We connect students and teachers <br>lorem ipsum dolor sit amet,<br> consectetur. </h2>
+            <!-- <span class=""><hr></span> -->
+           
+            <p class="lead">
+              <span class="fs-5 ">E</span>xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...
+            </p>
+          </div>
+          <div class="d-flex">
+          <div class="mb-5">
+            <h6  class="text-danger text-left fw-light pb-2">Our Vision</h6>
+            <h3>Consectetur adipisicing elit, sed do eiusmod tempor incididunt.</h3>
+          </div>
+          <div class="mb-5">
+            <h6 class="text-danger text-left fw-light pb-2">Our Mission</h6>
+            <h3>Adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</h3>
+          </div>
+          </div>
+          <div class="mb-5">
+            <img src="https://websitedemos.net/language-tutors-02/wp-content/uploads/sites/700/2020/09/about-us-01-free-img.jpg" alt="About Image" class="img-fluid">
+          </div>
+        </article>
+      </main>
+    </div>
+  </div>
+</div>
+
     
   `,
-  styles: ``
+  styles: `
+  .bg-about{
+      background-image: url('https://websitedemos.net/language-tutors-02/wp-content/uploads/sites/700/2020/09/bg-06-free-img.jpg');
+      background-size: cover;
+    
+      background-position: center;
+      background-attachment: fixed;
+      height: 80vh;
+      position: relative;
+}
+
+h2{
+  font-size:50px
+}
+h1{
+  font-size: 75px;
+}
+`
 })
 export class AboutComponent {
-  randomTextsd="Hi ";
+  randomTextsd = "Hi ";
 
 }
--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -13,11 +13,66 @@ import { FooterComponent } from "../../../shared/components/footer/footer.compon
 })
 export class HomeComponent {
   courses = [
-    { title: 'Course 1', description: 'Course 1 description goes here.' },
-    { title: 'Course 2', description: 'Course 2 description goes here.' },
-    { title: 'Course 3', description: 'Course 3 description goes here.' },
-    { title: 'Course 4', description: 'Course 4 description goes here.' },
-    { title: 'Course 5', description: 'Course 5 description goes here.' },
-    { title: 'Course 6', description: 'Course 6 description goes here.' },
+    { logo: 'C1', title: 'Course 1', description: 'Course 1 description goes here.' },
+    { logo: 'C2', title: 'Course 2', description: 'Course 2 description goes here.' },
+    { logo: 'C3', title: 'Course 3', description: 'Course 3 description goes here.' },
+    { logo: 'C4', title: 'Course 4', description: 'Course 4 description goes here.' },
+    { logo: 'C5', title: 'Course 5', description: 'Course 5 description goes here.' },
+    { logo: 'C6', title: 'Course 6', description: 'Course 6 description goes here.' },
+    { logo: 'C4', title: 'Course 4', description: 'Course 4 description goes here.' },
+    { logo: 'C5', title: 'Course 5', description: 'Course 5 description goes here.' },
+    { logo: 'C6', title: 'Course 6', description: 'Course 6 description goes here.' },
+     { logo: 'C4', title: 'Course 4', description: 'Course 4 description goes here.' },
+    { logo: 'C5', title: 'Course 5', description: 'Course 5 description goes here.' },
+    { logo: 'C6', title: 'Course 6', description: 'Course 6 description goes here.' },
+     { logo: 'C4', title: 'Course 4', description: 'Course 4 description goes here.' },
+    { logo: 'C5', title: 'Course 5', description: 'Course 5 description goes here.' },
+    { logo: 'C6', title: 'Course 6', description: 'Course 6 description goes here.' },
   ];
+
+  stepstoFindCourse=[
+    {
+      id:1,
+      head:"Find the perfect tutor",
+      description:"Elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Nam varius consectetur."
+    },
+    {
+      id:2,
+      head:"Enroll your lesson",
+      description:"Elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Nam varius consectetur."
+    },{
+      id:3,
+      head:"Start the journey",
+      description:"Elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Nam varius consectetur."
+    },
+  ]
+
+ // features
+features = [
+  {
+    imgTag: "assets/home/graduate.png",
+    title: "Expert Tutors",
+    description: "Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+  },
+  {
+    imgTag: "graduate.png",
+    title: "Verified Profiles",
+    description: "Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+  },
+  {
+    imgTag: "graduate.png",
+    title: "Pay Per Lesson",
+    description: "Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+  },
+  {
+    imgTag: "graduate.png",
+    title: "Affordable Prices",
+    description: "Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+  }
+];
+
+getAssetPath(filename: string): string {
+  return `assets/home/${filename}`;
+}
+
 }
