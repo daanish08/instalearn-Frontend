5da20b371e63d913a5f562fbdf4a3691cbfba8a3
MM109 <daanishm8@gmail.com>
2024-11-03 21:11:35 +0530

    added development page

---
+++ src/app/shared/components/development-page/development/development.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-development',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="container-fluid d-flex justify-content-center align-items-start  ">
+  <div class="text-center mb-5">
+    <img src="/assets/img/vectorIcons/construction.jpg" alt="Construction Image" class="img-fluid" style="height: 550px; width: auto;">
+    <h3 class="h2 text-muted  fw-light text-secondary">Exciting updates are on the way. <span class="fw-semibold text-success">Stay connected! </span></h3>
+  </div>
+</div>
+
+
+  `,
+  styles: ``
+})
+export class DevelopmentComponent {
+
+}
